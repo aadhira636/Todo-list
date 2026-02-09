c70eb4a7241b91055838f0394ca47c44ecc5cbdc
Santha Aadhira <74529097+aadhira636@users.noreply.github.com>
2026-02-09 15:58:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let todos = [
+  {
+    id: 1,
+    title: "First To-Do",
+  },
+  {
+    id: 2,
+    title: "Second To-Do",
+  },
+];
+
+function addToDo(event) {
+  console.log("hi");
+  event.preventDefault();
+  let input = document.getElementById("todo-input");
+  InputValue = input.value;
+  if (InputValue === "") {
+    // window.alert("Please write your task");
+    return;
+  }
+  todos.push({ id: todos.length + 1, title: InputValue });
+  input.value = "";
+  showTodos();
+}
+
+function showTodos() {
+  let toDoList = document.getElementById("todo-list");
+  toDoList.innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    toDoList.innerHTML += `<li>${todos[i].title}</li>`;
+  }
+}
+
+showTodos();
